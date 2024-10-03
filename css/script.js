17b0304e232ea08a8522b9ce4e770c999d5b0597
gimboSpaziale <gianmarco.fabbri3@studio.unibo.it>
2024-10-03 22:43:40 +0200

    added files relative to the first laboratory class

--- css/script.js
+++ css/script.js
@@ -5,3 +5,7 @@ themeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-theme');
     themeToggle.textContent = body.classList.contains('dark-theme') ? 'Tema Chiaro' : 'Tema Scuro';
 });
+
+function findMin(arr) {
+    return Math.min(...arr);
+}
